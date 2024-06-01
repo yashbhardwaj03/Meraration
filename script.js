efbe862d3027e1c223aae765bcc9559a984b3001
Yash Bhardwaj <yashbhardwaj42833@gmail.com>
2024-06-01 14:02:34 +0530

    very last commit

--- script.js
+++ script.js
@@ -6,15 +6,19 @@ document.addEventListener('DOMContentLoaded', () => {
     let results = document.querySelector(".results");
     let input = document.querySelector(".rc-number");
 
-    checkDetails.addEventListener("click", () => {
-        cards.style.display = "flex";
-        results.style.display = "flex";
-    });
-    reset.addEventListener("click", () => {
-        cards.style.display = "none";
-        results.style.display = "none";
-        input.value = ""; // Clear the input field
-    });
+    setTimeout(() => {
+        checkDetails.addEventListener("click", () => {
+            cards.style.display = "flex";
+            results.style.display = "flex";
+        });
+        reset.addEventListener("click", () => {
+            cards.style.display = "none";
+            results.style.display = "none";
+            input.value = ""; // Clear the input field
+        }); 
+    }, 1000);
+
+    
 });
 
 
